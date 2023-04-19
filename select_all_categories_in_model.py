e9c2e03c9bfbf99134288a4267e59ffa1305764b
Arthur Emig <arthur.erich.emig@gmail.com>
2023-04-19 20:48:24 +0200

    select categories works

--- select_all_categories_in_model.py
+++ select_all_categories_in_model.py
@@ -1,4 +1,5 @@
 import clr
+
 clr.AddReference('RevitAPI')
 from Autodesk.Revit.DB import *
 
@@ -14,22 +15,31 @@ cats = doc.Settings.Categories
 
 used_cat = []
 unused_cat = []
+cat_types = []
 count_error = 0
 for cat in cats:
-	#if cat.CategoryType == CategoryType.Model or cat.CategoryType == CategoryType.Annotation:
+    # if cat.CategoryType == CategoryType.Model or cat.CategoryType == CategoryType.Annotation:
     # print(cat.Id.IntegerValue)
     try:
-        cate = Revit.Elements.Category.ById(cat.Id.IntegerValue)
+        cate = cat
         count = FilteredElementCollector(doc).OfCategoryId(cat.Id).WhereElementIsNotElementType().GetElementCount();
-        if count>0:
-	        used_cat.append(cate)
+        if count > 0:
+            used_cat.append(cate)
         else:
             unused_cat.append(cate)
     except IndentationError:
-        print "Indentation"
+        print
+        "Indentation"
     except:
-    	print("Error Category Id is", cat.Id.IntegerValue)
-    	count_error = count_error + 1
+        print("Error Category Id is", cat.Name)
+
+        count_error = count_error + 1
+        cat_types.append(cat.CategoryType)
 print("number of errors: ", count_error)
+print("invalid cat types", list(set(cat_types)))
 
 OUT = used_cat, unused_cat
+
+print(len(used_cat))
+used_cat_names = [ct.Name for ct in used_cat]
+print(used_cat_names)
